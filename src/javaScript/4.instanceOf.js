7e8ee76d05da05cfdb1a3cc68a5265e08bbbfae3
lostsk <540000791@qq.com>
2022-02-11 18:02:51 +0800

    perf: instanceOf

--- src/javaScript/4.instanceOf.js
+++ src/javaScript/4.instanceOf.js
@@ -16,9 +16,29 @@ const myInstanceOf = function (obj, func) {
 let Fn = function () {}
 let p1 = new Fn()
 
-console.log(myInstanceOf({}, Object))
-console.log(myInstanceOf(p1, Fn))
-console.log(myInstanceOf({}, Fn))
-console.log(myInstanceOf(null, Fn))
-console.log(myInstanceOf(1, Fn))
-console.log(myInstanceOf(function a() {}, Function))
+// console.log(myInstanceOf({}, Object))
+// console.log(myInstanceOf(p1, Fn))
+// console.log(myInstanceOf({}, Fn))
+// console.log(myInstanceOf(null, Fn))
+// console.log(myInstanceOf(1, Fn))
+// console.log(myInstanceOf(function a() {}, Function))
+class Animal {
+  constructor(type) {
+    this.type = type
+  }
+  run() {
+    console.log(`${this.type} run!`)
+  }
+}
+class Pig extends Animal {
+  constructor(type, name) {
+    super(type)
+    this.name = name
+  }
+  eat() {
+    console.log(`${this.name} eating`)
+  }
+}
+const peiqi = new Pig('pig', 'peiqi')
+console.log(myInstanceOf(peiqi, Animal))
+console.log(peiqi.run())
